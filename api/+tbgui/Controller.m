55c323434ef90ff7f4f3fcc499b84ac790ebee2e
Markus Leuthold <github@titlis.org>
2026-07-31 10:09:11 +0200

    tbGui: deploy on double-click or pressing enter

--- api/+tbgui/Controller.m
+++ api/+tbgui/Controller.m
@@ -4,25 +4,36 @@ classdef Controller < handle
     %  SEE ALSO tbgui.View, tbgui.Model
     %
     %  2020 Markus Leuthold githubkusi@titlis.org
-    
+
     properties
         model
         view
     end
-    
+
     methods
         function self = Controller(model, view)
             self.model = model;
             self.view = view;
             self.init;
         end
-        
+
         function init(self)
             self.view.init(self, self.model);
             self.view.setToolboxNames(self.model.toolboxNames);
             self.view.setDefaultValues(self.model.prefs);
+            self.view.setWindowKeyPressFcn(@self.onKeyPress)
         end
-        
+
+        function onKeyPress(self, keyData)
+            if keyData.Key == "return"
+                self.useAndClose(self.view.getToolboxNames)
+            end
+        end
+
+        function onDoubleClick(self)
+            self.useAndClose(self.view.getToolboxNames)
+        end
+
         function useAndClose(self, toolboxNames)
             if tbGetPref(tbGetPersistentPrefs, 'verbose', true)
                 disp("selected: " + join(toolboxNames, ', '));
@@ -30,12 +41,12 @@ classdef Controller < handle
             self.model.use(toolboxNames);
             delete(self.view)
         end
-        
+
         function copyToClipboardAndClose(self, selectedToolbox)
             self.model.copyToClipboard(selectedToolbox);
             delete(self.view)
         end
-        
+
         function filteredToolboxNames = filterToolboxes(self, filterStr)
             filteredToolboxNames = self.model.filterToolboxes(filterStr);
         end
